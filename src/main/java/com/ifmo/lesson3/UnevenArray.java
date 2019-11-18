6fb81b47344d280ddf53b63b1b65002db9d7a3a5
grigoriewadim <grigoriewadim@yandex.ru>
2019-11-18 23:09:19 +0300

    lesson3 update

--- src/main/java/com/ifmo/lesson3/UnevenArray.java
+++ src/main/java/com/ifmo/lesson3/UnevenArray.java
@@ -1,44 +1,35 @@
 package com.ifmo.lesson3;
 
-import java.util.Arrays;
-import java.util.Collections;
-
 public class UnevenArray {
     /*
     Создайте массив из всех нечётных чисел от 1 до 99, выведите его на экран в строку, а затем
     этот же массив выведите на экран тоже в строку, но в обратном порядке (99 97 95 93 … 7 5 3
     1)
      */
-    private static int[] array = new int[100];
+    private static int[] array = new int[50];
 
     private static int[] create() {
-        for (int i = 1; i < 100; i++) {
-            if (i % 2 != 0) {
-                array[i] = i;
-            }
+        for (int i = 0; i < array.length; i++) {
+            array[i] = 2 * i + 1; //чтобы не было нуля
         }
         return array;
     }
 
-    private static int[]reverse() {
-        int l = array.length;
-        for (int j = 0; j < l / 2; j++) {
-            int temp = array[j];
-            array[j] = array[l - j - 1];
-            array[l - j - 1] = temp;
+    private static int[] reverse() {
+        int[] reversed = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            reversed[i] = array[array.length - 1 - i];
         }
-        return array;
-    }
-
-    private static void summArray(int[] array) {
-        create();
-        int[] newArray = new int[200];
-        newArray = Arrays.copyOf(array, array.length);
-
-
+        return reversed;
     }
 
     public static void main(String[] args) {
-//        summArray();
+        create();
+        for (int value : array) {
+            System.out.print(value + " ");
+        }
+        for(int reverseValue: reverse()) {
+            System.out.print(reverseValue + " ");
+        }
     }
 }
