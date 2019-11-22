fd1a5580cd247a45ed67ca61646795e9dae386ed
grigoriewadim <grigoriewadim@yandex.ru>
2019-11-22 13:40:09 +0300

    your final pizza

--- src/main/java/com/ifmo/lesson17/Pizza.java
+++ src/main/java/com/ifmo/lesson17/Pizza.java
@@ -1,4 +1,17 @@
 package com.ifmo.lesson17;
 
 public class Pizza {
+    private final String dough;
+    private final String cheese;
+    private final int catchup;
+    private final int tomatoes;
+    private final int pepperoni;
+
+    public Pizza(String dough, String cheese, int catchup, int tomatoes, int pepperoni) {
+        this.dough = dough;
+        this.cheese = cheese;
+        this.catchup = catchup;
+        this.tomatoes = tomatoes;
+        this.pepperoni = pepperoni;
+    }
 }
