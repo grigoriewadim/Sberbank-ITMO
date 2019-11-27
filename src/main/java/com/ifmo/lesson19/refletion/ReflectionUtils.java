3580506aa798b011bd705d9c12c88063d99a0650
grigoriewadim <grigoriewadim@yandex.ru>
2019-11-27 12:35:23 +0300

    add ReflectionUtils

--- src/main/java/com/ifmo/lesson19/refletion/ReflectionUtils.java
+++ src/main/java/com/ifmo/lesson19/refletion/ReflectionUtils.java
@@ -1,31 +1,37 @@
 package com.ifmo.lesson19.refletion;
 
 import com.ifmo.lesson6.ArrayList;
-
-import java.lang.reflect.Field;
+import java.io.*;
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 
-public class ReflectionUtils {
-    private static ArrayList listFields = new ArrayList();
-    private static ArrayList listMethods = new ArrayList();
-    ReflectionUtils(Object obj) {
-        for (Field fields : obj.getClass().getFields()) {
-                listFields.add(fields.getName());
+public class ReflectionUtils implements Serializable {
+
+    static String toString(Class obj) {
+        ArrayList result = new ArrayList();
+        while (obj != null) {
+            for (Method method : obj.getDeclaredMethods()) {
+                int modifiers = method.getModifiers();
+                if (Modifier.isPrivate(modifiers)) {
+                    result.add(method);
+                    System.out.println(method);
+                }
+            }
+            obj = obj.getSuperclass();
         }
-        Method[] methods = obj.getClass().getMethods();
-        for (Method met: methods) {
-            listMethods.add(met.getName());
-        }
+        return result.iterator().toString();
     }
 
-    public static void main(String[] args) {
-        new ReflectionUtils(new java.io.ByteArrayOutputStream());
-        for (Object fields: listFields) {
-            System.out.println(fields);
-        }
+    static <T> T deepClone(T obj) throws CloneNotSupportedException, IOException {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(stream);
+        oos.writeObject(obj);
+        oos.close();
+        return (T) obj.getClass();
+    }
 
-        for (Object methods: listMethods) {
-            System.out.println(methods);
-        }
+    public static void main(String[] args) throws IOException, CloneNotSupportedException {
+        toString(BufferedInputStream.class);
+        deepClone(BufferedInputStream.class);
     }
 }
