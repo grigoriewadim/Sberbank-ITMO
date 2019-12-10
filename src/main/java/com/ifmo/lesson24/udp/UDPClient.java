5131a82a2c79b24f410920493d086b4acd8077d3
grigoriewadim <grigoriewadim@yandex.ru>
2019-12-10 13:13:55 +0300

    lesson24

---
+++ src/main/java/com/ifmo/lesson24/udp/UDPClient.java
@@ -0,0 +1,40 @@
+package com.ifmo.lesson24.udp;
+
+import com.ifmo.lesson20.chat.IOUtils;
+
+import java.io.*;
+import java.net.*;
+
+import static java.lang.Thread.*;
+
+public class UDPClient {
+
+    private void createAndListenSocket() {
+        try {
+            DatagramSocket socket = new DatagramSocket();
+            InetAddress IPAddress = InetAddress.getByName("localhost");
+            byte[] read = new byte[1024];
+            File outFile = new File("D:\\!JAVA!\\Projects\\Sberbank-ITMO-master\\src\\main\\resources\\wap.txt");
+            FileInputStream fileInputStream = new FileInputStream(outFile);
+            byte[] outArray = fileInputStream.readAllBytes();
+            while (fileInputStream.read() != -1) {
+                DatagramPacket sendPacket = new DatagramPacket(outArray, outArray.length, IPAddress, 9876);
+                socket.send(sendPacket);
+            }
+            System.out.println("Отправлено");
+            DatagramPacket incomingPacket = new DatagramPacket(read, read.length);
+            socket.receive(incomingPacket);
+            String response = new String(incomingPacket.getData());
+            System.out.println("Ответ сервера: " + response);
+            sleep(2000);
+
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        UDPClient client = new UDPClient();
+        client.createAndListenSocket();
+    }
+}
---
+++ src/main/java/com/ifmo/lesson24/udp/UDPServer.java
@@ -0,0 +1,48 @@
+package com.ifmo.lesson24.udp;
+
+import java.io.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+public class UDPServer {
+
+    private void createAndListenSocket() {
+        try {
+            DatagramSocket socket = new DatagramSocket(9876);
+            byte[] incomingData = new byte[1024];
+            while (true) {
+                DatagramPacket incomingPacket = new DatagramPacket(incomingData, incomingData.length);
+                socket.receive(incomingPacket);
+                byte[] data = incomingPacket.getData();
+                ByteArrayInputStream in = new ByteArrayInputStream(data);
+                FileOutputStream output = new FileOutputStream("D:\\!JAVA!\\Projects\\Sberbank-ITMO-master\\src\\main\\resources\\receive.txt");
+                int bufferSize = 1024;
+                byte[] buffer = new byte[bufferSize];
+                int n;
+                n = in.read(buffer, 0, bufferSize);
+                while (n >= 0) {
+                    output.write(buffer, 0, n);
+                    n = in.read(buffer, 0, bufferSize);
+                }
+                InetAddress IPAddress = incomingPacket.getAddress();
+                int port = incomingPacket.getPort();
+                String reply = "Сообщение получено";
+                byte[] replyBytea = reply.getBytes();
+                DatagramPacket replyPacket =
+                        new DatagramPacket(replyBytea, replyBytea.length, IPAddress, port);
+                socket.send(replyPacket);
+                Thread.sleep(2000);
+                System.exit(0);
+            }
+
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        UDPServer server = new UDPServer();
+        server.createAndListenSocket();
+    }
+}
